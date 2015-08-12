210a1e070ffd438e720aa1ea156b660471baeec4
jesusbotias <jesus.botias@gmail.com>
2015-08-12 18:40:48 +0200

    Quiz 19. normalizar respuesta

--- models/quiz.js
+++ models/quiz.js
@@ -10,15 +10,15 @@ module.exports = function(sequelize, DataTypes) {
     'Quiz',
     { pregunta: {
         type: DataTypes.STRING,
-        validate: { notEmpty: {msg: "-> Falta Pregunta"}}
+        validate: { notEmpty: {msg: "--> Falta Pregunta"}}
       },
       respuesta: {
         type: DataTypes.STRING,
-        validate: { notEmpty: {msg: "-> Falta Respuesta"}}
+        validate: { notEmpty: {msg: "--> Falta Respuesta"}}
       },
 			tema: {
 				type: DataTypes.STRING,
-				validate: { notEmpty: {msg: "-> Falta Tema"}}
+				validate: { notEmpty: {msg: "--> Falta Tema"}}
 			}
     }
   );
